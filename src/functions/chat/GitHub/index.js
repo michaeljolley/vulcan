3738598634e434372bc1f20f255e69115a31a186
Michael Jolley <mike@sparcapp.io>
2019-11-07 15:37:49 -0600

    Adding a little goodness to GitHub chat function

--- src/functions/chat/GitHub/index.js
+++ src/functions/chat/GitHub/index.js
@@ -1,15 +1,38 @@
 module.exports = async function(context, req) {
-  context.log("JavaScript HTTP trigger function processed a request.");
+  // All chat functions will receive a payload of:
+  // {
+  //    channel: string,
+  //    chatUserState: {
+  //     badges?: Badges;
+  //     color?: string;
+  //     "display-name"?: string;
+  //     emotes?: { [emoteid: string]: string[] };
+  //     id?: string;
+  //     mod?: boolean;
+  //     turbo?: boolean;
+  //     'emotes-raw'?: string;
+  //     'badges-raw'?: string;
+  //     "room-id"?: string;
+  //     subscriber?: boolean;
+  //     'user-type'?: "" | "mod" | "global_mod" | "admin" | "staff";
+  //     "user-id"?: string;
+  //     "tmi-sent-ts"?: string;
+  //     flags?: string;
+  //     [paramater: string]: any;
+  // 'message-type'?: "chat" | "action" | "whisper";
+  // username?: string;
+  // bits?: string;
+  // },
+  //    message: string
+  // }
 
-  if (req.query.name || (req.body && req.body.name)) {
-    context.res = {
-      // status: 200, /* Defaults to 200 */
-      body: "Hello " + (req.query.name || req.body.name)
-    };
-  } else {
-    context.res = {
-      status: 400,
-      body: "Please pass a name on the query string or in the request body"
-    };
-  }
+  context.res = {
+    body: "Ok"
+  };
+
+  // Send a message to the SignalR service
+  const message =
+    "Mike's GitHub account can be found at https://github.com/michaeljolley";
+
+  // Send it
 };
