6c14217645c46b1e6b8e4e81e08060f423bea790
Michael Jolley <mike@sparcapp.io>
2019-11-17 21:39:04 -0600

    Working on Socket.IO hub configuration

--- src/services/hub/src/index.js
+++ src/services/hub/src/index.js
@@ -2,6 +2,10 @@ const app = require('express')();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 
+app.route('/', (req, res) => {
+  res.sendStatus(200);
+});
+
 io.on('connection', socket => {
   socket.on('onChatMessage', payload => {
     io.emit('onChatMessage', payload);
