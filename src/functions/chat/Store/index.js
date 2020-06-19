a3643195910148c0743d2d06c09d04a832aeb8ac
Mike Holloway <michael.a.holloway@outlook.com>
2020-06-19 09:59:24 -0700

    Adding !store command

---
+++ src/functions/chat/Store/index.js
@@ -0,0 +1,46 @@
+const io = require("socket.io-client");
+require("dotenv").config();
+
+const socket = io.connect(process.env.VULCANHUBURL);
+
+module.exports = async function(context, req) {
+  // All chat functions will receive a payload of:
+  // {
+  //    channel: string,
+  //    tags: {
+  //     badges?: Badges;
+  //     color?: string;
+  //     "display-name"?: string;
+  //     emotes?: { [emoteid: string]: string[] };
+  //     id?: string;
+  //     mod?: boolean;
+  //     turbo?: boolean;
+  //     'emotes-raw'?: string;
+  //     'badges-raw'?: string;
+  //     "room-id"?: string;
+  //     subscriber?: boolean;
+  //     'user-type'?: "" | "mod" | "global_mod" | "admin" | "staff";
+  //     "user-id"?: string;
+  //     "tmi-sent-ts"?: string;
+  //     flags?: string;
+  //     [paramater: string]: any;
+  // 'message-type'?: "chat" | "action" | "whisper";
+  // username?: string;
+  // bits?: string;
+  // },
+  //    message: string,
+  //    user: User
+  // }
+
+  const message =
+    "Want to support the BBB stream by repping the merch? Visit https://baldbeardedbuilder.threadless.com/ to get your gear.";
+
+  const payload = {
+    message,
+    messageType: "chat", // or 'whisper'
+    recipient: null // required when messageType === whisper
+  };
+
+  // Send a message to the Socket.io
+  socket.emit("newMessage", payload);
+};
